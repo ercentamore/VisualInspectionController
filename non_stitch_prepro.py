6d96f909968ae841aaf37e05f178efa9fa3bcaa7
ercentamore <ercentamore@crimson.ua.edu>
2026-04-02 11:07:57 -0500

    Update non_stitch_prepro.py

--- non_stitch_prepro.py
+++ non_stitch_prepro.py
@@ -471,7 +471,7 @@ def main(
                     )
         
                     circle_coords = keep_central_circles(
-                        circle_coords, tile_bgr_u8, x_clip=150, y_clip=150
+                        circle_coords, tile_bgr_u8, x_clip=0, y_clip=0
                     )
         
                     circle_coords = circle_coords[
@@ -491,7 +491,7 @@ def main(
                         print("Visualization Here")
                         fig, ax = plt.subplots(1,2,figsize=(10,5))
                         ax[0].imshow(image_pil,  cmap='gray', vmin=0, vmax=255)
-                        ax[0].set_title('Reference Image')
+                        # ax[0].set_title('Reference Image')
                         ax[0].axis('off')
 
                         ax[1].imshow(image_pil,  cmap='gray', vmin=0, vmax=255)
@@ -499,7 +499,7 @@ def main(
                             ax[1].scatter(*zip(*c_ref), c='r', s=50, marker='x')
                         else:
                             print("No corners found")
-                        ax[1].set_title('Reference Image with Markers')
+                        # ax[1].set_title('Reference Image with Markers')
                         ax[1].axis('off')
 
                         plt.tight_layout()
@@ -645,7 +645,7 @@ def main(
                         )
             
                         circle_coords = keep_central_circles(
-                            circle_coords, tile_bgr_u8, x_clip=150, y_clip=150
+                            circle_coords, tile_bgr_u8, x_clip=0, y_clip=0
                         )
             
                         circle_coords = circle_coords[
@@ -665,7 +665,7 @@ def main(
                             print("Visualization Here")
                             fig, ax = plt.subplots(1,2,figsize=(10,5))
                             ax[0].imshow(image_pil,  cmap='gray', vmin=0, vmax=255)
-                            ax[0].set_title('Reference Image')
+                            # ax[0].set_title('Reference Image')
                             ax[0].axis('off')
 
                             ax[1].imshow(image_pil,  cmap='gray', vmin=0, vmax=255)
@@ -673,7 +673,7 @@ def main(
                                 ax[1].scatter(*zip(*c_ref), c='r', s=50, marker='x')
                             else:
                                 print("No corners found")
-                            ax[1].set_title('Reference Image with Markers')
+                            # ax[1].set_title('Reference Image with Markers')
                             ax[1].axis('off')
 
                             plt.tight_layout()
