aff0e4dc16b74e81a7934c40d3662f3f1a1eb870
ercentamore <ercentamore@crimson.ua.edu>
2026-04-02 11:12:54 -0500

    Update non_stitch_prepro.py

--- non_stitch_prepro.py
+++ non_stitch_prepro.py
@@ -464,7 +464,7 @@ def main(
                     circle_coords = get_circles(
                         circle_kernel,
                         tile_bgr_f32,
-                        pos_thresh=20,
+                        pos_thresh=40,
                         neg_thresh=115,
                         pixels_to_shrink=10,
                         mode="soft",   # or "strict" if you pass per-channel thresholds
@@ -638,7 +638,7 @@ def main(
                         circle_coords = get_circles(
                             circle_kernel,
                             tile_bgr_f32,
-                            pos_thresh=20,
+                            pos_thresh=40,
                             neg_thresh=115,
                             pixels_to_shrink=10,
                             mode="soft",   # or "strict" if you pass per-channel thresholds
