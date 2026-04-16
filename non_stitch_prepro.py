3d361819483014bf3e969827cb47dfe21f5b28cd
ercentamore <ercentamore@crimson.ua.edu>
2026-04-16 14:53:48 -0500

    Update non_stitch_prepro.py

--- non_stitch_prepro.py
+++ non_stitch_prepro.py
@@ -634,7 +634,7 @@ def main(
                             circle_kernel,
                             tile_bgr_f32,
                             pos_thresh=32,
-                            neg_thresh=125,
+                            neg_thresh=105,
                             pixels_to_shrink=10,
                             mode="soft",   # or "strict" if you pass per-channel thresholds
                         )
