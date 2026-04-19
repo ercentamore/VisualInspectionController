4878a9184e8cc053079c7e9d6e897428c78abce6
ercentamore <ercentamore@crimson.ua.edu>
2026-04-19 16:18:14 -0500

    Update non_stitch_prepro.py

--- non_stitch_prepro.py
+++ non_stitch_prepro.py
@@ -636,7 +636,7 @@ def main(
                             tile_bgr_f32,
                             pos_thresh=29,
                             neg_thresh=81,
-                            pixels_to_shrink=1,
+                            pixels_to_shrink=0,
                             mode="soft",   # or "strict" if you pass per-channel thresholds
                         )
 
